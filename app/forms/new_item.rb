999afedc066065b3dac1cf9dff1d0ae2ae505bd4
Fifi <fifi.ding@iss.nthu.edu.tw>
2025-05-29 15:15:24 +0800

    feat: validate item type

--- app/forms/new_item.rb
+++ app/forms/new_item.rb
@@ -6,14 +6,19 @@ module LostNFound
   module Form
     # This form is used to validate items.
     class NewItem < Dry::Validation::Contract
-      config.messages.load_paths << File.join(__dir__, 'errors/new_project.yml')
+      config.messages.load_paths << File.join(__dir__, 'errors/new_item.yml')
 
       params do
-        required(:name).filled
-        optional(:type).maybe(format?: URI::DEFAULT_PARSER.make_regexp)
+        required(:type).filled(:integer)
+        required(:name).filled(:string)
         optional(:description).maybe(:string)
         optional(:location).maybe(:string)
-        optional(:contact).filled
+        optional(:person_info).maybe(:string)
+      end
+
+      rule(:type) do
+        allowed_types = [0, 1] # 0: lost, 1: found
+        key.failure(:type_valid?) unless allowed_types.include?(value)
       end
     end
   end
