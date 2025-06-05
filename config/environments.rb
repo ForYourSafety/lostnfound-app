53f77eb46ae8ee68def7fccc2b41ccc7143720f5
Yi Kuo <kuokuoyiyi@gmail.com>
2025-06-06 01:57:06 +0800

    fix: redis configuration

--- config/environments.rb
+++ config/environments.rb
@@ -23,6 +23,10 @@ module LostNFound
     Figaro.load
     def self.config = Figaro.env
 
+    # Secure session configuration
+    ONE_MONTH = 30 * 24 * 60 * 60
+    SecureMessage.setup(ENV.delete('MSG_KEY'))
+
     # HTTP Request logging
     configure :development, :production do
       plugin :common_logger, $stdout
@@ -35,18 +39,12 @@ module LostNFound
     # Allows binding.pry in dev/test and rake console in production
     require 'pry'
 
-    # Secure session configuration
-    ONE_MONTH = 30 * 24 * 60 * 60
-    @redis_url = ENV.delete('REDISCLOUD_URL')
-    SecureMessage.setup(ENV.delete('MSG_KEY'))
-    SecureSession.setup(@redis_url)
-
     # Console/Pry configuration
     configure :development, :test do
       logger.level = Logger::INFO
 
       # NOTE: env var REDIS_URL only used to wipe the session store (ok to be nil)
-      SecureSession.setup(ENV.fetch('REDIS_URL', nil)) # REDIS_URL used again below
+      SecureSession.setup(ENV.fetch('REDIS_URL', nil))
 
       # use Rack::Session::Cookie,
       #     expire_after: ONE_MONTH, secret: config.SESSION_SECRET
