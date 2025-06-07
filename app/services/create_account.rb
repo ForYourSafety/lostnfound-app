4f33c78dc03506a7fa63dc1b6b6a1748ed36735d
Fifi <fifi.ding@iss.nthu.edu.tw>
2025-06-07 16:04:39 +0800

    feat: signed message

--- app/services/create_account.rb
+++ app/services/create_account.rb
@@ -14,18 +14,13 @@ module LostNFound
       @config = config
     end
 
-    def call(username:, password:, email:)
-      message = {
-        username:,
-        password:,
-        email:
-      }
+    def call(email:, username:, password:)
+      account = { email:, username:, password: }
 
       response = HTTP.post(
         "#{@config.API_URL}/accounts/",
-        json: message
+        json: SignedMessage.sign(account)
       )
-
       raise InvalidAccount unless response.code == 201
     end
   end
