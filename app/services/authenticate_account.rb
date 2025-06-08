75dfbcfe7201887144ad595228f04b59c8ad9458
Sarah-Pan <sarah2950@gmail.com>
2025-06-08 21:07:27 +0800

    chore: adapt authenticate_account.rb

--- app/services/authenticate_account.rb
+++ app/services/authenticate_account.rb
@@ -8,13 +8,9 @@ module LostNFound
     class NotAuthenticatedError < StandardError; end
     class ApiServerError < StandardError; end
 
-    def initialize(config)
-      @config = config
-    end
-
     def call(username:, password:)
       credentials = { username: username, password: password }
-      response = HTTP.post("#{@config.API_URL}/auth/authenticate",
+      response = HTTP.post("#{ENV['API_URL']}/auth/authenticate",
                            json: SignedMessage.sign(credentials))
       raise(NotAuthenticatedError) if response.code == 401
       raise(ApiServerError) if response.code != 200
